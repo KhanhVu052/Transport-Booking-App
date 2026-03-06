0aa8ce56228d294232d0a6d31833fcfd686bd9dc
danielvu052 <danielvu052@gmail.com>
2026-03-06 11:22:54 +0700

    feat: implement JWT authentication and introspect API

--- backend/src/main/java/com/khanhvu/booking_system/controller/AuthenticationController.java
+++ backend/src/main/java/com/khanhvu/booking_system/controller/AuthenticationController.java
@@ -3,8 +3,11 @@ package com.khanhvu.booking_system.controller;
 
 import com.khanhvu.booking_system.dto.request.ApiResponse;
 import com.khanhvu.booking_system.dto.request.AuthenticationRequest;
+import com.khanhvu.booking_system.dto.request.IntrospectRequest;
 import com.khanhvu.booking_system.dto.respone.AuthenticationResponse;
+import com.khanhvu.booking_system.dto.respone.IntrospectResponse;
 import com.khanhvu.booking_system.service.AuthenticationService;
+import com.nimbusds.jose.JOSEException;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
@@ -12,6 +15,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.text.ParseException;
 
 @RestController
 @RequestMapping("/auth")
@@ -22,11 +27,19 @@ public class AuthenticationController {
 
     @PostMapping("/log-in")
     ApiResponse<AuthenticationResponse> authenticate(@RequestBody AuthenticationRequest request){
-        boolean result = authenticationService.authenticate(request);
+        var result = authenticationService.authenticate(request);
         return ApiResponse.<AuthenticationResponse>builder()
-                .result(AuthenticationResponse.builder()
-                        .authenticated(result)
-                        .build())
+                .result(result)
+                .build();
+    }
+
+    @PostMapping("/introspect")
+    public ApiResponse<IntrospectResponse> introspect(@RequestBody IntrospectRequest request)
+            throws ParseException, JOSEException {
+        var result = authenticationService.introspectResponse(request);
+
+        return ApiResponse.<IntrospectResponse>builder()
+                .result(result)
                 .build();
     }
 
--- backend/src/main/java/com/khanhvu/booking_system/dto/request/IntrospectRequest.java
+++ backend/src/main/java/com/khanhvu/booking_system/dto/request/IntrospectRequest.java
@@ -1,4 +1,5 @@
-package com.khanhvu.booking_system.dto.respone;
+package com.khanhvu.booking_system.dto.request;
+
 
 import lombok.*;
 import lombok.experimental.FieldDefaults;
@@ -9,7 +10,7 @@ import lombok.experimental.FieldDefaults;
 @Builder
 @FieldDefaults(level = AccessLevel.PRIVATE)
 
-public class AuthenticationResponse {
+
+public class IntrospectRequest {
     String token;
-    boolean authenticated;
 }
