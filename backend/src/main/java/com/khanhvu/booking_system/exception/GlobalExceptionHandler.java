43ea619d4a07264242e5ee6ceedce283e83c5708
danielvu052 <danielvu052@gmail.com>
2026-03-16 14:49:32 +0700

    feat: integrate JwtAuthenticationEntryPoint and update ErrorCode status

--- backend/src/main/java/com/khanhvu/booking_system/exception/GlobalExceptionHandler.java
+++ backend/src/main/java/com/khanhvu/booking_system/exception/GlobalExceptionHandler.java
@@ -3,6 +3,7 @@ package com.khanhvu.booking_system.exception;
 
 import com.khanhvu.booking_system.dto.request.ApiResponse;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -28,7 +29,21 @@ public class GlobalExceptionHandler {
 
         apiResponse.setCode(errorCode.getCode());
         apiResponse.setMessage(errorCode.getMessage());
-        return ResponseEntity.badRequest().body(apiResponse);
+        return ResponseEntity
+                .status(errorCode.getStatusCode())
+                .body(apiResponse);
+    }
+
+    @ExceptionHandler(value = AccessDeniedException.class)
+    ResponseEntity<ApiResponse> handlingAccessDeniedExpcetion(AccessDeniedException exception){
+        ErrorCode errorCode = ErrorCode.UNAUTHORIZED;
+
+        return ResponseEntity.status(errorCode.getStatusCode()).body(
+                ApiResponse.builder()
+                        .code(errorCode.getCode())
+                        .message(errorCode.getMessage())
+                        .build()
+        );
     }
 
     @ExceptionHandler(value = MethodArgumentNotValidException.class)
